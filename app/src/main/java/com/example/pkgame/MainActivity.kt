f5a1419cc98cccb6a20b65e9b7901b7cdc8fe8d2
tosh7 <zlia.6.lj.425@gmail.com>
2019-11-05 18:25:48 +0900

    added gameActivity

--- app/src/main/java/com/example/pkgame/MainActivity.kt
+++ app/src/main/java/com/example/pkgame/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.pkgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        start_button.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
     }
 }
