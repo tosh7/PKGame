85ea078cdbaa8e659afce92722b25b6b0d87f71f
tosh7 <zlia.6.lj.425@gmail.com>
2019-11-05 20:48:39 +0900

    enabled to check isGaled

--- app/src/main/java/com/example/pkgame/GameActivity.kt
+++ app/src/main/java/com/example/pkgame/GameActivity.kt
@@ -1,12 +1,32 @@
 package com.example.pkgame
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_game.*
+import kotlin.random.Random
 
 class GameActivity : AppCompatActivity() {
+
+    val buttonArray: Array<Button> by lazy {
+        arrayOf(top_right_button, top_left_button, bottom_right_button, bottom_left_button)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        val random = Random.nextInt(0,4)
+
+        for ((index, button) in buttonArray.withIndex()) {
+            button.setTag(index)
+            button.setOnClickListener {
+                val result =  (random == button.tag)
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("result", result)
+                startActivity(intent)
+            }
+        }
     }
 }
